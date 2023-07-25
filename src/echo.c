67657724844d00f3f7d6530292448c7e41f22d9e
Nick van den Dungen <nicktvdd@hotmail.com>
2023-07-25 14:08:38 +0300

    added 42header, split into efficient functions

--- src/echo.c
+++ src/echo.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   echo.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nvan-den <nvan-den@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/25 13:40:15 by nvan-den          #+#    #+#             */
+/*   Updated: 2023/07/25 14:07:22 by nvan-den         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
-static	int	nb_argv(char **argv)
+static int	nb_argv(char **argv)
 {
 	int		size;
 
@@ -9,30 +21,28 @@ static	int	nb_argv(char **argv)
 		size++;
 	return (size);
 }
-
+void	n_function(char **argv)
+{
+	int	i;
+	
+	while (argv[i] && ft_strcmp(argv[i], "-n") == 0)
+		i++;
+	while (argv[i])
+	{
+		ft_putstr_fd(argv[i], 1);
+		if (argv[i + 1] && argv[i][0] != '\0')
+			write(1, " ", 1);
+		i++;
+	}
+}
 int	ft_echo(char **argv)
 {
 	int		i;
-	int		n_option;
 
-	i = 1;
-	n_option = 0;  // check "-n" new line option in echo
-	if (nb_argv(argv) > 1)
-	{
-		while (argv[i] && ft_strcmp(argv[i], "-n") == 0)
-		{
-			n_option = 1;
-			i++;
-		}
-		while (argv[i])
-		{
-			ft_putstr_fd(argv[i], 1);
-			if (argv[i + 1] && argv[i][0] != '\0')
-				write(1, " ", 1);
-			i++;
-		}
-	}
-	if (n_option == 0)
+	i = 1;  
+	if (nb_argv(argv) > 1)// check "-n" new line option in echo
+		n_function(argv);
+	else
 		write(1, "\n", 1);
 	return (0);
 }
