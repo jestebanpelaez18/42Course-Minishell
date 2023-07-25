b3ac415965091807cf4b5018cab0275a66e074b5
Nick van den Dungen <nicktvdd@hotmail.com>
2023-07-25 16:45:10 +0300

    revert to putendl

--- src/error_exit.c
+++ src/error_exit.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 12:03:02 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/07/25 14:13:15 by nvan-den         ###   ########.fr       */
+/*   Updated: 2023/07/25 16:45:02 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,11 @@
 
 void	error_msg(char *msg)
 {
-	ft_putstr_fd(msg, 2);
+	ft_putendl_fd(msg, 2);
 	exit(EXIT_FAILURE);
 }
 
 void	error_msg_noexit(char *msg)
 {
-	ft_putstr_fd(msg, 2);
+	ft_putendl_fd(msg, 2);
 }
