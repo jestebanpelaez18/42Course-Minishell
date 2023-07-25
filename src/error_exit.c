345eb80372fe7664920f7ea9c328e257d6a787e2
Nick van den Dungen <nicktvdd@hotmail.com>
2023-07-25 14:13:08 +0300

    replaced putendl with putchar

--- src/error_exit.c
+++ src/error_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   error_exit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nvan-den <nvan-den@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/28 12:03:02 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/07/05 15:38:16 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/07/25 14:12:58 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ void	error_msg(char *msg)
 
 void	error_msg_noexit(char *msg)
 {
-	ft_putendl_fd(msg, 2);
+	ft_putstr_fd(msg, 2);
 }
