224b1aed1c2f61e249a4b03a88086db27be26a62
Nick van den Dungen <nicktvdd@hotmail.com>
2023-07-26 16:09:36 +0300

    Revert "copy struc"
    This reverts commit 6a31bce841c252a2c300137d1b405815779d7fde.

--- parse_redic.c
+++ parse_redic.c
@@ -6,7 +6,7 @@
 /*   By: jpelaez- <jpelaez-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 19:43:55 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/07/26 15:17:53 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/07/25 19:59:22 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,19 +44,19 @@
 // 	if (tok_temp->type == LEFT_R || tok_temp->type == RIGH_R
 // 		|| tok_temp->type == HEREDOC || tok_temp->type == APPEND)
 // 		create_single_redirection(tok_temp, new_redireccion);
-// 	// parse_redirection(node);
+// 	parse_redirection(node);
 // }
 
-// t_token	*check_redirection(t_token *node)
-// {
-// 	t_token	*tok_temp;
+t_token	*check_redirection(t_token *node)
+{
+	t_token	*tok_temp;
 
-// 	tok_temp = node;
-// 	while (tok_temp && tok_temp->type == WORD)
-// 		tok_temp = tok_temp->next;
-// 	if (!tok_temp || tok_temp->type == PIPE)
-// 		return (node);
-// 	// else if (tok_temp->type == LEFT_R || tok_temp->type == RIGH_R
-// 	// 	|| tok_temp->type == HEREDOC || tok_temp->type == APPEND)
-// 	return (tok_temp);
-// }
+	tok_temp = node;
+	while (tok_temp && tok_temp->type == WORD)
+		tok_temp = tok_temp->next;
+	if (!tok_temp || tok_temp->type == PIPE)
+		return (node);
+	// else if (tok_temp->type == LEFT_R || tok_temp->type == RIGH_R
+	// 	|| tok_temp->type == HEREDOC || tok_temp->type == APPEND)
+	return (tok_temp);
+}
