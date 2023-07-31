ae843596d46a3c40898a68e1abc4c4ed53a44bb4
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-07-31 20:14:20 +0300

    working in the expander

--- obj/error_exit.d
+++
@@ -1,8 +0,0 @@
-obj/error_exit.o: src/error_exit.c inc/minishell.h inc/../libft/libft.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/history.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/rlstdc.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/rltypedefs.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/readline.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/keymaps.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/chardefs.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/tilde.h
--- obj/executor.d
+++
@@ -1,8 +0,0 @@
-obj/executor.o: src/executor.c inc/minishell.h inc/../libft/libft.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/history.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/rlstdc.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/rltypedefs.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/readline.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/keymaps.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/chardefs.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/tilde.h
