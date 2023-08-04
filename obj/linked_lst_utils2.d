2aad6dc9f958fbc41cb1710ff09e1e5c98752311
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-08-04 19:03:11 +0300

    expander partially done

--- obj/linked_lst_utils2.d
+++
@@ -1,9 +0,0 @@
-obj/linked_lst_utils2.o: src/linked_lst_utils2.c inc/minishell.h \
-  inc/../libft/libft.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/history.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/rlstdc.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/rltypedefs.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/readline.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/keymaps.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/chardefs.h \
-  /Users/jpelaez-/.brew/opt/readline/include/readline/tilde.h
