676f395f53ab28905e21174f02362d892cd46b9d
Nick van den Dungen <nicktvdd@hotmail.com>
2023-08-10 16:29:30 +0300

    so close to being finished

--- src/executor.c
+++ src/executor.c
@@ -6,7 +6,7 @@
 /*   By: nvan-den <nvan-den@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 17:51:54 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/08/10 15:09:45 by nvan-den         ###   ########.fr       */
+/*   Updated: 2023/08/10 16:25:00 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,17 +41,17 @@ void	close_all_fd(int num_pipes, int (*pipes)[2], int current_pipe)
 	}
 }
 
-int	piper(t_cmd *cmds, t_redirec *redirec, t_data *data)
+int	execute_pipes(t_cmd *cmds, int num_pipes, int (*pipes)[2], int current_pipe)
 {
-	int pids[data->pipex];
-	int	pipes[data->pipex][2];
+	int pids[data->pipex + 1];
+	int	pipes[data->pipex + 1][2];
 	int	i;
 	int	error;
 	
 	i = 0;
 	redirec = NULL;//
 	cmds = NULL;//
-	create_pipes(data->pipex, pipes);
+	create_pipes(data->pipex + 1, pipes);
 	while (i < data->pipex)
 	{
 		pids[i] = fork();
@@ -59,22 +59,24 @@ int	piper(t_cmd *cmds, t_redirec *redirec, t_data *data)
 			error_msg("Error creating process\n");
 		if (pids[i] == 0)
 		{//child process
-			close_all_fd(data->pipex, pipes, i);
+			close_all_fd(data->pipex + 1, pipes, i);
 			if (i > 0)
 			{
 				if (dup2(pipes[i - 1][0], STDIN_FILENO) == -1)
 			//receive the variable from last pipe
-					error_msg("Error at dup\n");
+					error_msg("Error at dup read\n");
+				close(pipes[i][0]);
 			}
-			if (i < data->pipex - 1)
+			if (i < data->pipex)
 			{
 				if (dup2(pipes[i][1], STDOUT_FILENO) == -1)
 				//write the variable into the next pipe
-						error_msg("Error at dup\n");
+						error_msg("Error at dup write\n");
 			}
 			/* do something to the variable here! */
 			// if built-in -> which builtin -> run builtin()
 			//if execve
+			printf("hi\n");
 			char *argv[] = {"echo hi\n", NULL};
 			execvp("echo", argv);
 			error_msg("Error executing command\n");
@@ -82,9 +84,9 @@ int	piper(t_cmd *cmds, t_redirec *redirec, t_data *data)
 		i++;
 	}
 	//main process
-	close_all_fd(data->pipex, pipes, -1);
+	close_all_fd(data->pipex + 1, pipes, -1);
 	i = 0;
-	while (i < data->pipex)
+	while (i < data->pipex + 1)
 	{
 		waitpid(pids[i],&error ,0); //&=addres of random int variable necessary for cathing errors etc. and other return values (signals etc).
 		i++;
@@ -95,21 +97,17 @@ int	piper(t_cmd *cmds, t_redirec *redirec, t_data *data)
 
 void	executor(t_data *data)
 {
-	t_pid *pid;
-
+	int	i;
+	int pipes[data->pipex][2];
+	
+	i = 0;
 	signal_in_exec();
 	//data->struc_cmd = expander(data, data->struc_cmd);
-	pid = data->struc_pid;
-	//pid->id = fork();
-/* 	if (pid->id == 0)
-	{ */
-		piper(data->struc_cmd, data->redirections, data);
-			//launch_single_cmd(data->struc_cmd, data->redirections, data);
-		// else
-		// 	pipes
-		/*Here we have to check the stuff with the pipes,
-		because we have multiple cmd,
-		i Will test with one command because is easie
-	and the we can test for multiple commands*/
-/* 	} */
+	create_pipes_recursive(data->pipex, pipes, 0);
+	execute_pipes(data->struc_cmd, data->pipex, pipes, 0);
+	while (i < data->pipex);
+	{
+		close(pipes[i][0]);
+		close(pipes[i][1]);
+	}
 }
