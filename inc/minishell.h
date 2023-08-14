1daf63d9ee3b145bc0a1ef8b368d47c93ca39fcc
Kimpozer <composerjunhee@gmail.com>
2023-08-14 17:00:45 +0300

    cd working

--- inc/minishell.h
+++ inc/minishell.h
@@ -89,7 +89,7 @@ void 					reset(t_data *data);
 
 /*Builtins*/
 int						ft_pwd(void);
-int						ft_cd(char **args, t_data *data);
+int						ft_cd(char **args);
 /*Signal functions*/
 void					signal_in_exec(void);
 void					start_signal(void);
