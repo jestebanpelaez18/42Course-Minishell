2965c1749fbe1bd4c551371f677a8b2a223b85d2
Rasmus Rask <rasse.rask@gmail.com>
2023-08-16 12:59:46 +0300

    echo done, testing edge cases combined with minishell

--- src/echo.c
+++ src/echo.c
@@ -6,19 +6,29 @@
 /*   By: rrask <rrask@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 13:40:15 by nvan-den          #+#    #+#             */
-/*   Updated: 2023/08/15 14:06:09 by rrask            ###   ########.fr       */
+/*   Updated: 2023/08/16 12:59:02 by rrask            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minishell.h"
+// #include "minishell.h"
 
 void	n_function(char **argv)
 {
 	int	i;
+	int	n_flag;
 
-	i = 0;
-	while (argv[i] && ft_strncmp(argv[i], "-n", 4) == 0)
+	i = 1;
+	n_flag = 0;
+	if (!argv || (argv[i] && (strncmp(argv[i], "echo", 4) != 0)))
+	{
+		return ;
+	}
+	i++;
+	while (argv[i] && (strncmp(argv[i], "-n", strlen(argv[i])) == 0))
+	{
+		n_flag = 1;
 		i++;
+	}
 	while (argv[i])
 	{
 		ft_putstr_fd(argv[i], 1);
@@ -26,6 +36,8 @@ void	n_function(char **argv)
 			write(1, " ", 1);
 		i++;
 	}
+	if (n_flag == 0)
+		write(1, "\n", 1);
 }
 
 int	ft_echo(char **argv)
@@ -33,9 +45,9 @@ int	ft_echo(char **argv)
 	int		i;
 
 	i = 1;
-	if (ft_strlen(*argv) > 1)// check "-n" new line option in echo
+	if (!argv)
+		return (-1);
+	if (strlen(*argv) > 1)
 		n_function(argv);
-	else
-		write(1, "\n", 1);
 	return (0);
 }
