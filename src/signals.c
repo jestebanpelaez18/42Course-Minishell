fd03dd3da085389439b2e705cc68f30fa2f5691a
Juan Esteban Pelaez Hoyos <jpelaez-@student.hive.fi>
2023-08-24 16:27:14 +0300

    signals working for all the cases

--- src/signals.c
+++ src/signals.c
@@ -6,12 +6,11 @@
 /*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 09:37:14 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/08/24 15:49:39 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/08/24 16:26:08 by jpelaez-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
 
 static void	handler(int signal)
 {
@@ -23,9 +22,6 @@ static void	handler(int signal)
 		rl_redisplay();
 	}
 }
-
-/* initial signals, when shell is interactive*/
-
 void	start_signal(void)
 {
 	if (signal(SIGINT, handler) == SIG_ERR)
@@ -39,6 +35,13 @@ static void	fork_signal(int signal)
 	if (signal == SIGQUIT)
 	{
 		kill(-2, signal);
+		ft_putendl_fd("Quit: 3", STDOUT_FILENO);
+		rl_on_new_line();
+		rl_replace_line("", 1);
+		g_var.g_exit_status = 131;
+	}
+	if (signal == SIGINT)
+	{
 		ft_putendl_fd("", STDOUT_FILENO);
 		rl_on_new_line();
 		rl_replace_line("", 1);
@@ -49,12 +52,14 @@ void	signal_in_exec(void)
 {
 	if (signal(SIGQUIT, fork_signal) == SIG_ERR)
 		error_msg("Signal error");
+	if (signal(SIGINT, fork_signal) == SIG_ERR)
+		error_msg("Signal error");
 }
 
-void heredoc_signal(void)
+void	heredoc_signal(void)
 {
 	if (signal(SIGINT, hd_handler) == SIG_ERR)
 		error_msg("Signal error");
 	if (signal(SIGQUIT, SIG_IGN) == SIG_ERR)
-		error_msg("Signal error");	
+		error_msg("Signal error");
 }
