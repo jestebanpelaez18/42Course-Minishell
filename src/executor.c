0123b76b8879534f9716224d9bb2f5941e288fa8
Nick van den Dungen <nicktvdd@hotmail.com>
2023-08-29 14:36:56 +0300

    executor no norm error

--- src/executor.c
+++ src/executor.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executor.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jpelaez- <jpelaez-@student.hive.fi>        +#+  +:+       +#+        */
+/*   By: nvan-den <nvan-den@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 14:16:39 by jpelaez-          #+#    #+#             */
-/*   Updated: 2023/08/28 19:14:13 by jpelaez-         ###   ########.fr       */
+/*   Updated: 2023/08/29 14:35:44 by nvan-den         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,5 @@ void	executor(t_data *data)
 	if (data->pipex == 0)
 		launch_single_cmd(data->struc_cmd, data);
 	else
-	 	launch_pipes(data);
-	/*Here we have to check the stuff with the pipes,
-		because we have multiple cmd,
-		i Will test with one command because is easie
-	and the we can test for multiple commands*/
+		launch_pipes(data);
 }
- 
